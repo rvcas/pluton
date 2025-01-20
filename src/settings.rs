07953628ffbaf621bc04fedaa4da87bec84fb8be
Pi Lanningham <pi.lanningham@gmail.com>
2025-01-20 01:32:24 -0500

    fix: make the app compile on linux

--- src/settings.rs
+++ src/settings.rs
@@ -1,14 +1,27 @@
 use iced::{window, Size};
 
-pub fn window() -> window::Settings {
-    let platform_specific = window::settings::PlatformSpecific {
+#[cfg(target_os = "linux")]
+pub fn platform_specific() -> window::settings::PlatformSpecific {
+    window::settings::PlatformSpecific::default()
+}
+
+#[cfg(target_os = "windows")]
+pub fn platform_specific() -> window::settings::PlatformSpecific {
+    window::settings::PlatformSpecific::default()
+}
+
+#[cfg(target_os = "macos")]
+pub fn platform_specfic() -> window::settings::PlatformSpecific {
+    window::settings::PlatformSpecific {
         titlebar_transparent: true,
         fullsize_content_view: true,
         title_hidden: false,
     };
+}
 
+pub fn window() -> window::Settings {
     window::Settings {
-        platform_specific,
+        platform_specific: platform_specific(),
         size: Size::new(2048.0, 1536.0),
         ..window::Settings::default()
     }
