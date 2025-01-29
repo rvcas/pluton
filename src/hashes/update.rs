a274340d781b99a5cc812e011a7b5a6966929ff6
rvcas <x@rvcas.dev>
2025-01-28 20:44:26 -0500

    chore: fix all warnings

--- src/hashes/update.rs
+++ src/hashes/update.rs
@@ -35,25 +35,26 @@ impl State {
 
     fn update_hashes(&mut self) {
         let contents = self.text();
-        if contents.len() == 0 {
+
+        if contents.is_empty() {
             self.clear_hashes();
             self.warning = Some("Empty String".to_string());
             return;
         }
 
         let contents = match self.encoding.clone().unwrap_or(detect_encoding(&contents)) {
-            Encoding::Hex => match hex::decode(&contents.replace('\n', "")) {
+            Encoding::Hex => match hex::decode(contents.replace('\n', "")) {
                 Ok(raw) => raw,
                 Err(e) => {
-                    self.warning = Some(format!("Invalid hex: {}", e.to_string()));
+                    self.warning = Some(format!("Invalid hex: {}", e));
                     self.clear_hashes();
                     return;
                 }
             },
-            Encoding::Base64 => match BASE64_STANDARD.decode(&contents.replace('\n', "")) {
+            Encoding::Base64 => match BASE64_STANDARD.decode(contents.replace('\n', "")) {
                 Ok(raw) => raw,
                 Err(e) => {
-                    self.warning = Some(format!("Invalid base64: {}", e.to_string()));
+                    self.warning = Some(format!("Invalid base64: {}", e));
                     self.clear_hashes();
                     return;
                 }
@@ -63,11 +64,17 @@ impl State {
 
         self.blake2b_224 = hex::encode(Hasher::<224>::hash(&contents));
         self.blake2b_256 = hex::encode(Hasher::<256>::hash(&contents));
+
         let mut sha512 = sha2::Sha512::new();
+
         sha512.update(&contents);
+
         self.sha512 = hex::encode(sha512.finalize());
+
         let mut sha256 = sha2::Sha256::new();
+
         sha256.update(&contents);
+
         self.sha256 = hex::encode(sha256.finalize());
     }
 
